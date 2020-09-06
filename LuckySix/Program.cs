f21b72781286c4f82d49cc8826b93802c72e4918
Slavko Petrovic <slavko_petrovic@live.com>
2020-09-06 08:25:07 +0200

    Add project files.

---
+++ LuckySix/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LuckySix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] testArray = new int[] { 1, 1, 2, 3 };
+         
+            Console.WriteLine(LuckySix(testArray));
+        }
+
+        public static bool LuckySix(int[] inputNumbers)
+        {
+            bool isConditionSatisfied = false;
+            int sum = 0;
+            int targetValue = 6;
+            int len = inputNumbers.Length;
+            int newValue;
+            int oldValue;
+            int bufferLength = 3;
+            Queue<int> buffer = new Queue<int>();
+            bool isBufferLoaded = false;
+            // Init buffer queue
+            for(int i = 0; i < bufferLength; i++)
+            {
+                buffer.Enqueue(0);
+            }
+
+            for (int j = 0; !isConditionSatisfied && j < len; j++)
+            {
+                oldValue = buffer.Dequeue();
+                newValue = inputNumbers[j];
+                buffer.Enqueue(newValue);
+                sum += newValue - oldValue;
+
+                if(!isBufferLoaded && j >= bufferLength - 1)
+                {
+                    isBufferLoaded = true;
+                }
+
+                if(isBufferLoaded && sum == targetValue)
+                {
+                    isConditionSatisfied = true;
+                }
+            }
+            
+            return isConditionSatisfied;
+
+        }
+    }
+}
